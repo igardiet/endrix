965ccd3559b2898a0cbc8f02ccdaec5f86ef45e9
igardiet <igardiet@gmail.com>
2023-09-29 17:53:57 +0200

    Header components tag wrapped in div classes

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,18 @@
+import Header from "@/components/Header";
+
 export default function Home ()
 {
   return (
-    <h1 className="text-green-500">Endrix</h1>
+    <div className="
+    bg-neutral-900 
+      rounded-lg 
+      h-full
+      w-full
+      overflow-hidden
+      overflow-y-auto">
+      <Header>
+        Header
+</Header>
+    </div>
   )
 }
