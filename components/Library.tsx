2ebe95b2b5a571807c75e60ecfacfbd3af77b85b
igardiet <igardiet@gmail.com>
2023-09-29 15:48:09 +0200

    your library p tag classes

--- components/Library.tsx
+++ components/Library.tsx
@@ -9,7 +9,11 @@ const Library = () =>
             <div className="flex items-center justify-between px-5 pt-4">
                 <div className="inline-flex items-center gap-x-2">
                     <TbPlaylist className="tect-neutral-400" size={ 26 } />
+                    <p className="text-neutral-400 font-medium-text-md">
+                        Your Library
+                    </p>
                 </div>
+                
             </div>
         </div>
     )
