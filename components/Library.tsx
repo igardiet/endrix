f688e927320bbd25bfefe2e05595e145e127bda9
igardiet <igardiet@gmail.com>
2023-09-29 15:31:07 +0200

    div structure and its classes containing TbPlaylist icon tag

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { TbPlaylist } from 'react-icons/tb';
+
 const Library = () =>
 {
     return (
-        <div>Library</div>
+        <div className="flex flex-col">
+            <div className="flex items-center justify-between px-5 pt-4">
+                <div className="inline-flex items-center gap-x-2">
+                    <TbPlaylist className="tect-neutral-400" size={ 26 } />
+                </div>
+            </div>
+        </div>
     )
 }
 
