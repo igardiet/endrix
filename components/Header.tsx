8c390d92d882365232a30c4012ae871e8cba4ed0
igardiet <igardiet@gmail.com>
2023-09-30 16:03:26 +0200

    feat: RxCaretLeft arrow button wrapped by custom divs

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft } from 'react-icons/rx';
 
 interface HeaderProps
 {
@@ -22,8 +24,28 @@ const Header: React.FC<HeaderProps> =
         }
 
         return (
-            <div>
-
+            <div className={ twMerge
+                ( `
+                    h-fit
+                    bg-gradient-to-b
+                    from-emerald-800
+                    p-6
+                `, className ) }>
+                <div className="w-full mb-4 flex items-center justify-between">
+                    <div className="hidden md:flex gap-x-2 items-center">
+                        <button className="
+                            rounded-full
+                            bg-black
+                            flex
+                            items-center
+                            justify-center
+                            hover:opacity-75
+                            transition
+                            ">
+                            <RxCaretLeft className="text-white" size={ 35 } />
+                        </button>
+                    </div>
+                </div>
             </div>
         )
     }
