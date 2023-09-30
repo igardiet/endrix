87198e71fed7a6bdbb414801059fc001704ad240
igardiet <igardiet@gmail.com>
2023-09-30 16:00:43 +0200

    chore: file text formatted

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,7 @@ export default function Home ()
 {
   return (
     <div className="
-    bg-neutral-900 
+      bg-neutral-900 
       rounded-lg 
       h-full
       w-full
@@ -12,7 +12,7 @@ export default function Home ()
       overflow-y-auto">
       <Header>
         Header
-</Header>
+      </Header>
     </div>
   )
 }
