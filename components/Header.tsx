74451cecd4c231522fcd88f6bcb1b2d63927a27c
igardiet <igardiet@gmail.com>
2023-09-30 16:55:41 +0200

    feat: sign up, BiSearch, HiHome button icons set and wrapped by correspondent divs

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,10 @@
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
+import Button from "./Button";
 
 interface HeaderProps
 {
@@ -60,8 +64,42 @@ const Header: React.FC<HeaderProps> =
                             <RxCaretRight className="text-white" size={ 35 } />
                         </button>
                     </div>
-                    <div>
-                        
+                    <div className="flex md:hidden gap-x-2 items-center">
+                        <button
+                            className="
+                                rounded-full
+                                p-2
+                                bg-white
+                                flex 
+                                items-center 
+                                justify-center
+                                hover:opacity-75 
+                                transition
+                                ">
+                            <HiHome className="text-black" size={ 20 } />
+                        </button>
+                        <button
+                            className="
+                                rounded-full
+                                p-2
+                                bg-white
+                                flex 
+                                items-center 
+                                justify-center
+                                hover:opacity-75 
+                                transition
+                                ">
+                            <BiSearch className="text-black" size={ 20 } />
+                        </button>
+                    </div>
+                    <div className="flex justify-between items-center gap-x-4">
+                        <>
+                            <div>
+                                <Button>
+                                    Sign up
+                                </Button>
+                            </div>
+                        </>
                     </div>
                 </div>
             </div>
