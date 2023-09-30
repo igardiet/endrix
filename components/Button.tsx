ccab45b458e64fc3c7b26e74c990bae6f20486e3
igardiet <igardiet@gmail.com>
2023-09-30 16:51:09 +0200

    feat: button forwardRef props set

---
+++ components/Button.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> { }
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+    ( {
+        className,
+        children,
+        disabled,
+        type = 'button',
+        ...props
+    }, ref ) =>
+    {
+        return (
+            <button>
+
+            </button>
+        )
+    }
+)
+
+Button.displayName = 'Button'
+
+export default Button;
