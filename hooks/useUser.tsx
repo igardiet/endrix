719b09dd8ee2a16cef38647ed7d1c0db072b03de
igardiet <igardiet@gmail.com>
2023-10-03 16:07:47 +0200

    feat: myUserContextProvider state variables setted

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,5 +1,6 @@
 import { User } from "@supabase/auth-helpers-nextjs";
-import { createContext } from "react";
+import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react";
+import { createContext, useState } from "react";
 import { Subscription, UserDetails } from "@/types";
 
 type UserContextType =
@@ -8,13 +9,29 @@ type UserContextType =
         user: User | null;
         userDetails: UserDetails | null;
         isLoading: boolean;
-        subscription: Subscription | null
+        subscription: Subscription | null
     }
 
 export const UserContext = createContext<UserContextType | undefined>( undefined )
 
 export interface Props
-{ 
-    [propName: string]: any
+{
+    [ propName: string ]: any
 }
 
+export const MyUserContextProvider = ( props: Props ) =>
+{
+    const
+        {
+            session,
+            isLoading: isLoadingUser,
+            supabaseClient: supabase
+        } = useSessionContext();
+    const user = useSupaUser();
+    const accessToken = session?.access_token ?? null;
+    const [ isLoadingData, setIsLoadingData ] = useState( false );
+    const [ userDetails, setUserDetails ] = useState<UserDetails | null>( null );
+    const [ subscription, setSubscription ] = useState<Subscription | null>( null );
+
+    const getUserDetails = () => supabase.from( 'users' ).select( '*' ).single();
+}
