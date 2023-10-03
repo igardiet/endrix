b0c3982a0fb7481c64f475c12b28d992fdc3ecdc
igardiet <igardiet@gmail.com>
2023-10-03 14:45:46 +0200

    feat: Subscription interface

--- types.ts
+++ types.ts
@@ -36,3 +36,23 @@ export interface Price
     metadata?: Stripe.Metadata;
     products?: Product;
 }
+
+export interface Subscription
+{
+    id: string;
+    user_id: string;
+    status?: Stripe.Subscription.Status;
+    metadata?: Stripe.Metadata;
+    price_id?: string;
+    quantity?: string;
+    cancel_at_period_end?: boolean;
+    created: string;
+    current_period_start: string;
+    current_period_end: string;
+    ended_at?: string;
+    cancel_at?: string;
+    canceled_at?: string;
+    trial_start?: string;
+    trial_end?: string;
+    prices?: Price;
+}
