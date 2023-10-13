c9f8b3ef8bbd9442e2a48ea376620507e200f158
igardiet <igardiet@gmail.com>
2023-10-13 13:25:31 +0200

    feat: sidebar and ModalProvider component wrapped by UserProvider tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import Sidebar from '@/components/Sidebar'
 import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
 import './globals.css'
 
 const font = Figtree( { subsets: [ 'latin' ] } )
@@ -21,9 +23,12 @@ export default function RootLayout ( {
     <html lang="en">
       <body className={ font.className }>
         <SupabaseProvider>
-          <Sidebar>
-            { children }
-          </Sidebar>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>
+              { children }
+            </Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
