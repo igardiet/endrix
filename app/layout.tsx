9c07cdf26676dffddb274fdc3743c2612eadf50b
igardiet <igardiet@gmail.com>
2023-11-30 12:45:32 +0100

    feat: ToasterProvider tag located

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
-import Sidebar from '@/components/Sidebar'
-import SupabaseProvider from '@/providers/SupabaseProvider'
-import UserProvider from '@/providers/UserProvider'
-import ModalProvider from '@/providers/ModalProvider'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Figtree } from 'next/font/google';
+
+import Sidebar from '@/components/Sidebar';
+import SupabaseProvider from '@/providers/SupabaseProvider';
+import UserProvider from '@/providers/UserProvider';
+import ModalProvider from '@/providers/ModalProvider';
+import ToasterProvider from '@/providers/ToasterProvider';
+
+import './globals.css';
 
 const font = Figtree( { subsets: [ 'latin' ] } )
 
@@ -22,6 +25,7 @@ export default function RootLayout ( {
   return (
     <html lang="en">
       <body className={ font.className }>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
